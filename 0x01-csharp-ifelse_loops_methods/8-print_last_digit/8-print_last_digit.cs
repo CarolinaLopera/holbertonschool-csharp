cd55deafce4bd0a837edadc04f0123ae3ea96912
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-12 23:56:53 -0500

    last digit in positive

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+class Number {
+    public static int PrintLastDigit(int number) {
+        if (number < 0) {
+            number = number * - 1;
+        }
+        return (number % 10);
+    }
+}
