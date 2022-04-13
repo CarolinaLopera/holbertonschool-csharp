19db84c0eab6a877c0e9878a1911e14d1a6319fb
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-12 23:40:43 -0500

    only islower

--- 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -11,20 +11,20 @@ class Character {
     }
 }
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        char[] letters = { 'a', 'A', 'Q', 'h', '9', 'B', 'g'};
-        int i;
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         char[] letters = { 'a', 'A', 'Q', 'h', '9', 'B', 'g'};
+//         int i;
 
-        for (i = 0; i < letters.Length; i++)
-        {
-            if(Character.IsLower(letters[i]))
-                Console.WriteLine("{0} is lowercase", letters[i]);
-            else
-                Console.WriteLine("{0} is uppercase", letters[i]);
-        }
-    }
+//         for (i = 0; i < letters.Length; i++)
+//         {
+//             if(Character.IsLower(letters[i]))
+//                 Console.WriteLine("{0} is lowercase", letters[i]);
+//             else
+//                 Console.WriteLine("{0} is uppercase", letters[i]);
+//         }
+//     }
 
-} 
+// } 
