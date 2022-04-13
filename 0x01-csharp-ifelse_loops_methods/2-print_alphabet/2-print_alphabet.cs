d769e2ba879dd71871eb35faeabdd4cf545ac554
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-12 20:21:35 -0500

    Print alphabet

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            // for (int i = 0; i < alphabet.Length; i++) {
+            //     Console.Write("{0}", alphabet[i]);
+            // }
+            Console.Write("abcdefghijklmnopqrstuvwxyz");
+        }
+    }
+}
