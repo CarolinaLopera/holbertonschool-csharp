c52aa0596b64e7aa0e47001255f5955989358128
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-13 00:11:40 -0500

    print n times _

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Line {
+    public static void PrintLine(int length) {
+        if (length == 0 || length < 0) {
+            Console.WriteLine();
+            return;
+        }
+        for (int i = 1; i <= length; i++) {
+            Console.Write("_");
+            if (i == length) {
+                Console.WriteLine();
+            }
+        }
+    }
+}
