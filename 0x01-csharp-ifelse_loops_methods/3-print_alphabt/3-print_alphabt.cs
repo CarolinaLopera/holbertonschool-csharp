65bdc0faf561ec30d799ea78c9e7f29563637ce8
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-12 20:52:31 -0500

    without e and q

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char alphabet = 'a';
+
+            for (int i = 0; i < 26; i++) {
+                if (alphabet != 'e' && alphabet != 'q') {
+                    Console.Write("{0}", alphabet);
+                }
+                alphabet++;
+            }
+        }
+    }
+}
