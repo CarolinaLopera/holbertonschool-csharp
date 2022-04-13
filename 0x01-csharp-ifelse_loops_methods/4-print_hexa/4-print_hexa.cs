426ca781b6a750aea09cf9d068440b594375fdef
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-12 21:46:08 -0500

    print 0xhex

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 0; num < 99; num++){
+                string hex = Convert.ToString(num, 16);
+                Console.Write("{0} = 0x{1}\n", num, hex);
+            }
+        }
+    }
+}
