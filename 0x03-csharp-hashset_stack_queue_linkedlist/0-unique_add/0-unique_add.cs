3509effb433bf924190c9e422fedf05bdb6d181f
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-14 12:08:44 -0500

    Sum all integers uniqies of the list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+class List {
+    public static int Sum(List<int> myList) {
+        List<int> unique = myList.Distinct().ToList();
+        return unique.Sum();
+    }
+}
