6b4d1004b25284bd86a2748fd71485e353fec12b
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-13 22:28:40 -0500

    Create and print list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class List {
+	public static List<int> CreatePrint(int size) {
+		if (size < 0) {
+		    Console.WriteLine("Size cannot be negative");
+		    return null;
+		} else if (size == 0) {
+		    Console.WriteLine();
+		}
+
+		List<int> list = new List<int>();
+		for (int i = 0; i < size; i++) {
+		    list.Add(i);
+		    if (i != size - 1) {
+		        Console.Write($"{i} "); 
+		    } else {
+		        Console.Write($"{i}\n"); 
+		    }
+		}
+		return list;
+	}
+}
