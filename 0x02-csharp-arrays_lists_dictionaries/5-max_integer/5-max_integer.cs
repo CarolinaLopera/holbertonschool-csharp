d188aae503ce059556b5e7036cd31fc6d15e7010
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-13 22:00:01 -0500

    Integer max

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class List {
+    public static int MaxInteger(List<int> myList) {
+        if (myList.Length == 0) {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+        return (myList.OrderByDescending(x => x).First());
+    }
+}
+
