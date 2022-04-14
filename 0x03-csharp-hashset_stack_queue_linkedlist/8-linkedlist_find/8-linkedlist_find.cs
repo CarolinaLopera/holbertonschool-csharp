1c6a046e23745091856926e286603c8c7056e596
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-14 18:20:14 -0500

    value in index of linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static int FindNode(LinkedList<int> myLList, int value) {
+        int count = 0;
+        foreach (int i in myLList) {
+            if (i == value) {
+                return count;
+            }
+            count++;
+        }
+
+        return -1;
+    }
+}
