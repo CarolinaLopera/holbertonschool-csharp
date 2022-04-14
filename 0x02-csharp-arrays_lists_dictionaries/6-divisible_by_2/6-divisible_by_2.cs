79023902775f01d6b4ae115596d0d797c5130ab9
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-13 23:04:21 -0500

    Multiplos de 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List {
+    public static List<bool> DivisibleBy2(List<int> myList) {
+        List<bool> mod2 = new List<bool>();
+        for (int i = 0; i < myList.Count; i++) {
+            if (myList[i] % 2 == 0) {
+                mod2.Add(true);
+            } else {
+                mod2.Add(false);
+            }
+        }
+        return mod2;
+    }
+}
