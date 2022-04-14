9d3b580c0df00360cff91003c22733c36a983ba5
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-14 17:33:05 -0500

    Queue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue {
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search) {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        
+        if (aQueue.Count == 0) {
+            Console.WriteLine("Queue is empty");
+        } else {
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        }
+
+        aQueue.Enqueue(newItem);
+        if (aQueue.Contains(search)) {
+            Console.WriteLine("Queue contains \"{0}\": True", search);
+            bool flag = false;
+
+            for (int i = aQueue.Count; i > 0; i ++) {
+                aQueue.Dequeue();
+                if (flag == true) {
+                    break;
+                } else if (aQueue.Peek() == search) {
+                    flag = true;
+                }
+            }
+        } else {
+            Console.WriteLine("Queue contains \"{0}\": False", search);
+        }
+        return aQueue;
+    }
+}
