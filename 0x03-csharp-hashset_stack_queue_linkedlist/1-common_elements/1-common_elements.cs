c6b4277893bb82cff8df90258c0a99f8665f3e17
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-14 13:40:22 -0500

    Common integers in a list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static List<int> CommonElements(List<int> list1, List<int> list2) {
+        List<int> common = new List<int>();
+        for (int i = 0; i < list1.Count; i++) {
+            for (int j = 0; j < list1.Count; j++) {
+                if (list1[i] == list2[j]) {
+                    common.Add(list1[i]);
+                }
+            }
+        }
+        common.Sort();
+        return common;
+    }
+}
