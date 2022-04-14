9738744db3d78a368e2e2e1c1cc07a60cc5b8b48
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-13 23:17:09 -0500

    Validate if index < 0

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 class List {
     public static List<int> DeleteAt(List<int> myList, int index) {
-        if (index >= myList.Count) {
+        if (index >= myList.Count || index < 0) {
             Console.WriteLine("Index is out of range");
             return myList;
         }
