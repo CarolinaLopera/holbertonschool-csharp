3f4d6927e7c784fcd7118c1f2d670799de7aaf1f
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-14 19:17:31 -0500

    get value of node and remove first node

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList {
+    public static int Pop(LinkedList<int> myLList) {
+        if (myLList.Count == 0) {
+            return 0;
+        }
+
+        LinkedListNode<int> node = myLList.First;
+        myLList.RemoveFirst();
+        return node.Value;
+    }
+}
