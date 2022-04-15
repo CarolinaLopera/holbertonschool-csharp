eabd97c165721e2ad1c1fd63982c47d22f5ee466
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-14 22:58:39 -0500

    Linked list node in index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static int GetNode(LinkedList<int> myLList, int n) {
+        LinkedListNode<int> index = myLList.First;
+        for (int i = 0; index != null; i++) {
+            if (i == n) {
+                return index.Value;
+            }
+            index = index.Next;
+        }
+
+        return 0;
+    }
+}
