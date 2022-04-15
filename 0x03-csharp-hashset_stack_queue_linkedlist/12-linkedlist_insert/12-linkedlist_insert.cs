e2acbebce5208a5945de3f2a8c9bd8abe3e0234a
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-15 00:34:09 -0500

    Add node by value in sorted list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n) {
+        LinkedListNode<int> node = myLList.First;
+        while (node != null) {
+            if (node.Value >= n) {
+                myLList.AddBefore(node, n);
+                return node.Next;
+            }
+            node = node.Next;
+        }
+        return null;
+    }
+}
