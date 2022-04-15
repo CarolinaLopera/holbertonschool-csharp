58c0a83a3e52f85a654aeed4f02febf85f43ca1e
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-14 23:28:57 -0500

    Add all data of linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList {
+    public static int Sum(LinkedList<int> myLList) {
+        LinkedListNode<int> index = myLList.First;
+        int result = 0;
+        for (int i = 0; index != null; i++) {
+            result += index.Value;
+            index = index.Next;
+        }
+
+        return result;
+    }
+}
