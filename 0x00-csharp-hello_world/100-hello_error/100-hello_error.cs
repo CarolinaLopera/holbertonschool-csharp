8c9efa1ec070e05680bdc21f80e858209b949137
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-15 12:06:04 -0500

    stderr

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+        Environment.ExitCode = 1;
+    }
+}
