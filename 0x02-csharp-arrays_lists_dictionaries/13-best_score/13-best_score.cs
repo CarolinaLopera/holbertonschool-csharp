30befd19ef000dc09499caa3990f154d13c0910b
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-17 13:27:18 -0500

    Fix is dictionary is empty

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -5,11 +5,8 @@ using System.Collections.Generic;
 
 class Dictionary {
     public static string BestScore(Dictionary<string, int> myList) {
-        if (myList.Count == 0) {
-            return null;
-        }
         int biggest = -1;
-        string key = "";
+        string key = "None";
         foreach (KeyValuePair<string, int> i in myList) {
             if (i.Value > biggest) {
                 biggest = i.Value;
