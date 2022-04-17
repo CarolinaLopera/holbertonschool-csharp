f5b57fcd8a4ce5cddb0ed485d3533d5664b021cd
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-17 13:41:17 -0500

    Array two-dimensional

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        int[,] matrix = {
+                            {0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0},
+                            {0, 0, 1, 0, 0},
+                            {0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0}
+                        };
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j]);
+                if (j != matrix.GetLength(1) - 1) {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
