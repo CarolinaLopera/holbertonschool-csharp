2601214caa580282d85ea719708512aab2faab47
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-17 13:14:06 -0500

    Get the big value in dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+class Dictionary {
+    public static string BestScore(Dictionary<string, int> myList) {
+        int biggest = 0;
+        string key = "";
+        foreach (KeyValuePair<string, int> i in myList) {
+            if (i.Value > biggest) {
+                biggest = i.Value;
+                key = i.Key;
+            }
+        }
+        return key;
+    }
+}
