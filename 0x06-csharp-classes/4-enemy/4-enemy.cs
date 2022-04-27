b0ff5029e7769760c8095220b86609899f0c517c
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-26 23:06:12 -0500

    4, plubic property Name, get and set

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Enemies {
+    /// <summary> class Zombie </summary>
+    public class Zombie {
+        /// <summary> privates fields health and name </summary>
+        private int health;
+        private string name = "(No name)";
+
+        /// <summary> First constructor </summary>
+        public Zombie() {
+            this.health = 0;
+        }
+
+        /// <summary> public property Name </summary>
+        public string Name {
+            get {
+                return name;
+            }
+            set {
+                name = value;
+            }
+        }
+        
+        /// <summary> Second constructor, value = health </summary>
+        public Zombie(int value) {
+            this.health = value;
+            if (value < 0) {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+
+        /// <summary> public method of Zombie </summary>
+        public int GetHealth() {
+            return this.health;
+        }
+    }
+}
