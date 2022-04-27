5b2890bbd2a8857ee05009c41325a8b016a92022
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-26 22:42:09 -0500

    Exercise 3, method in Zombie class

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Enemies {
+    /// <summary> namespace Enemies </summary>
+    public class Zombie {
+        /// <summary> class Zombie with constructor </summary>
+        private int health;
+        /// <summary> private field health </summary>
+
+        public Zombie() {
+            this.health = 0;
+        }
+        
+        /// <summary> field value = health </summary>
+        public Zombie(int value) {
+            this.health = value;
+            if (value < 0) {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+
+        /// <summary> public method of Zombie </summary>
+        public int GetHealth() {
+            return this.health;
+        }
+    }
+}
