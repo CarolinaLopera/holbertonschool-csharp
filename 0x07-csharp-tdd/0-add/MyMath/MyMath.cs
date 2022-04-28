05aecfb8c69070d66df5e92157605959db602066
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-28 16:10:22 -0500

    Create all

---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyMath {
+    /// <sumary> class to add two numbers </sumary>
+    public class Operations {
+        /// <sumary> method of class </sumary>
+        public static int Add(int a, int b) {
+            return a + b;
+        }
+    }
+}
