8c534b206972034491ad67f63a36f9388bec31a0
CarolinaLopera <carolinalopera08@gmail.com>
2022-05-02 15:47:40 -0500

    Is subclass

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <sumary> class to check object </sumary>
+class Obj {
+        /// <sumary> mecthod to check if object
+        /// is instance of subclass </sumary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType) {
+        if (derivedType.IsSubclassOf(baseType))
+            return true;
+        return false;
+    }
+}
