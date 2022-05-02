354ed1dd9bb439ee209c7b376b5740ef4f2434bf
CarolinaLopera <carolinalopera08@gmail.com>
2022-05-02 15:29:13 -0500

    Add docs

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 
+/// <sumary> class to check object </sumary>
 class Obj {
+    /// <sumary> mecthod to check if object is Array </sumary>
     public static bool IsInstanceOfArray(object obj) {
         if (obj is Array)
             return true;
