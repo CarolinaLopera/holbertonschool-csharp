b21db85f5bed670e2dcdc5df8e7639df97241cde
CarolinaLopera <carolinalopera08@gmail.com>
2022-05-02 15:39:00 -0500

    Add documentation

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 
+/// <sumary> class to check object </sumary>
 class Obj {
+    /// <sumary> mecthod to check if object is integer </sumary>
     public static bool IsOfTypeInt(object obj) {
         if (obj.GetType() == typeof(int))
             return true;
