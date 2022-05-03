44133b0b1d3af265c4c56480850d395d763d796d
CarolinaLopera <carolinalopera08@gmail.com>
2022-05-02 22:56:43 -0500

    Exception

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+/// <sumary> class Shape </sumary>
+class Shape {
+    /// <sumary> method that generate a exception </sumary>
+    public virtual int Area() {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
