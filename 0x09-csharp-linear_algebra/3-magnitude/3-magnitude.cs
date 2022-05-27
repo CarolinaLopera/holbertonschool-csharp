175ea635af6256e255867ac60cf642277c75b667
CarolinaLopera <carolinalopera08@gmail.com>
2022-05-26 23:28:56 -0500

    Add three vectors

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -6,14 +6,12 @@ class VectorMath {
     public static double Magnitude(double[] vector) {
         if (vector.Length == 2) {
             double add = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2);
-            double root = Math.Sqrt(add);
-            double result = Math.Round(root);
+            double result = Math.Round(Math.Sqrt(add));
             return result;
         }
         if (vector.Length == 3) {
             double add = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2);
-            double root = Math.Sqrt(add);
-            double result = Math.Round(root);
+            double result = Math.Round(Math.Sqrt(add));
             return result;
         }
         return -1;
