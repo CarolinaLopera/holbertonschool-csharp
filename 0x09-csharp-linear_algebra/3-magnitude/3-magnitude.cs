64e326ed65f2a482c1f7a24df28e5c77663d5b26
CarolinaLopera <carolinalopera08@gmail.com>
2022-05-26 23:25:30 -0500

    magnus

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <sumary> class VectorMath </sumary>
+class VectorMath {
+    /// <sumary> method Magnitude </sumary>
+    public static double Magnitude(double[] vector) {
+        if (vector.Length == 2) {
+            double add = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2);
+            double root = Math.Sqrt(add);
+            double result = Math.Round(root);
+            return result;
+        }
+        if (vector.Length == 3) {
+            double add = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2);
+            double root = Math.Sqrt(add);
+            double result = Math.Round(root);
+            return result;
+        }
+        return -1;
+    }
+}
