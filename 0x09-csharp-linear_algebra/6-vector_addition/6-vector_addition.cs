a4b421100b40f98c79e77ccc85e5948274431277
CarolinaLopera <carolinalopera08@gmail.com>
2022-05-26 23:41:59 -0500

    Add vectors

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <sumary> class VectorMath </sumary>
+class VectorMath {
+    /// <sumary> method Add </sumary>
+    public static double[] Add(double[] vector1, double[] vector2) {
+        if (vector1.Length != vector2.Length){
+            double[] newVector = { -1 };
+            return newVector;
+        }
+        if (vector1.Length == 2) {
+            double[] newVector = { vector1[0] + vector2[0], vector1[1] + vector2[1] };
+            return newVector;
+        }
+        if (vector1.Length == 3) {
+            double[] newVector = { vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2] };
+            return newVector;
+        }
+        double[] newVector = { -1 };
+        return newVector;
+    }
+}
