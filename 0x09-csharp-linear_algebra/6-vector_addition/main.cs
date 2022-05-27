6e09beb4be78f1bc19a020c19b1e3fb5185bf28a
CarolinaLopera <carolinalopera08@gmail.com>
2022-05-27 00:24:10 -0500

    ...

--- 0x09-csharp-linear_algebra/6-vector_addition/main.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/main.cs
@@ -2,14 +2,16 @@ using System;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        double[] matrix = { -9, 4 };
-        double[] matrix2 = { 2, 2 };
-        double[] matrix1 = VectorMath.Add(matrix, matrix2);
-        foreach (var item in matrix1)
-        {
-            Console.WriteLine(item);
-        }
+        double[] vector1 = { 1, -8 };
+        double[] vector2 = { -7, 0 };
+        double[] result;
+
+        result = VectorMath.Add(vector1, vector2);
+
+        Console.WriteLine("(" + result[0] + ", " + result[1] + ")");
+        // Console.WriteLine("(" + result[0] + ", " + result[1] + ", " + result[2] + ")");
+        // Console.WriteLine("(" + result[0] + ")");
     }
 }
