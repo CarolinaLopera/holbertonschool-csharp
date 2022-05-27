418640aa57b02075816c94cb57fddecb0c1cefaf
CarolinaLopera <carolinalopera08@gmail.com>
2022-05-27 01:01:03 -0500

    fix 9

--- 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -6,10 +6,10 @@ class VectorMath {
     ///<summary> method Multiply </summary>
     public static double[] Multiply(double[] vector, double scalar) {
         if (vector.Length == 2) {
-            double[] result = new double[] { vector[0] * scalar, vector[1] * scalar }  ;
+            double[] result = new double[] { vector[0] * scalar, vector[1] * scalar };
             return result;
         } else if (vector.Length == 3) {
-            double[] result = new double[] { vector[0] * scalar, vector[1] * scalar, vector[2] * scalar }  ;
+            double[] result = new double[] { vector[0] * scalar, vector[1] * scalar, vector[2] * scalar };
             return result;
         }
         return -1;
