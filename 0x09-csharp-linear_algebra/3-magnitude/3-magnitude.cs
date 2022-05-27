45d25b2271b861fcd5f023078ce53328bada89ae
CarolinaLopera <carolinalopera08@gmail.com>
2022-05-27 00:43:48 -0500

    Fix 3

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -6,11 +6,11 @@ class VectorMath {
     public static double Magnitude(double[] vector) {
         if (vector.Length == 2) {
             double add = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2);
-            return (Math.Sqrt(add));
+            return (Math.Round(Math.Sqrt(add), 2));
         }
         if (vector.Length == 3) {
             double add = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2);
-            return (Math.Sqrt(add));
+            return (Math.Round(Math.Sqrt(add), 2));
         }
         return -1;
     }
---
+++ 0x09-csharp-linear_algebra/3-magnitude/main.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Windows;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] vector = { 0, 0 };
+
+        Console.WriteLine(VectorMath.Magnitude(vector));
+    }
+}
